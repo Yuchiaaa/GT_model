ee7fe3c984219f0e9e2f6d737771a77de3fcf14c
Wendy van Wooning <133221669+WJvanWooning@users.noreply.github.com>
2025-11-24 00:33:40 +0100

    Minor fix strategy table

--- gt.py
+++ gt.py
@@ -33,9 +33,7 @@ FIXED_ENV_STRATEGIES = {
     "Ambitious":       [0, 0, 1, 1, 0],  # Win-Stay, Lose-Shift. Repeats move if payoff was high, switches if payoff was low.
     "NegativeTFT":       [1, 0, 1, 0, 1],  # Suspicious Tit-For-Tat. Same behavior as TfT but starts by Defecting.
     "Opposing":        [1, 1, 0, 1, 0],  # Defects against cooperators, but cooperates if punished by defection.
-    
-    # s8 is the same as s10, the alternating strategy. I would propose, for example, a 'mean one': [0(/1), 1, 0, 0, 0]
-    "Mean One":       [0, 1, 1, 0, 0],  # Starts nice, but occasionally defects to test for exploitability.
+    "Mean":       [0, 1, 0, 0, 0],  # Starts nice, but occasionally defects to test for exploitability.
     "Forgiving":   [0, 0, 1, 0, 0],  # Retaliates against Defection but attempts to restore cooperation after mutual defection.
     "Alternating":       [0, 1, 1, 0, 0],  # Follows a fixed cycle regardless of the opponent's actions.
     "Diversifying":    [0, 1, 0, 0, 1]   # Win-shift, lose-stay. Switches action when winning, keeps action when losing.
